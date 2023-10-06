f0e24c32f89d38298459436913c0bda1cf8d1633
kim hoehwan <74888623+Kim-HoeHwan@users.noreply.github.com>
2023-10-06 11:17:55 +0900

    과제5.c

---
+++ 과제5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num, i;
+
+    printf("Please enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 2; i < num; i++) {
+        if (num % i == 0)
+            break;
+    }
+
+    if (i == num)
+        printf("It is a prime number.");
+    else
+        printf("It is not a prime number.");
+
+    return 0;
+}
