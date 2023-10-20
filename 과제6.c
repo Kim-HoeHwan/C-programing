634dbe74502d0838dbe4d903df34df34f9915551
kim hoehwan <74888623+Kim-HoeHwan@users.noreply.github.com>
2023-10-20 10:35:48 +0900

    과제6.c

---
+++ 과제6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int num = 5;
+	int i, j;
+	for (i = 0; i < num; i++) {
+		for (j = num - 1; j > i; j--) {
+			printf(" ");
+		}
+
+		for (j = 0; j < 2 * i + 1; j++) {
+			printf("*");
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
